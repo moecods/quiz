a67d752d078ecb3a2349d14ba8ae28ceb64575cb
moecods <jakmesh96@gmail.com>
2024-06-23 20:43:42 +0330

    feat: add quiz struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module moecods/quiz
 
 go 1.22.3
+
+require go.mongodb.org/mongo-driver v1.15.1 // indirect
--- main.go
+++ main.go
@@ -1,45 +1,34 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
-	"os"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+type Quiz struct {
+	ID primitive.ObjectID
+	Title string
+	Description string
+	Questions []Question
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
 
 type Question struct {
 	title string
-	answer string
+	correct_answer string
+	user_answer string
 }
 
 func main() {
-	// first read a csv file (quiz question)
-	file, err := os.Open("quiz_1.csv")
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-	defer file.Close()
+	quiz := Quiz{
+		Title: "english A1",
+		Description: "General Knowledge English",
+		CreatedAt:  time.Now(),
+		UpdatedAt: time.Now(),
+	} 
 
-	reader := csv.NewReader(file)
-
-	records, err := reader.ReadAll()
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	var questions []Question
-	for _, record := range records {
-		questions = append(questions, Question{
-			title: record[0],
-			answer: record[1],
-		})
-    }
-
-	fmt.Println(questions)
-	// second put in the question struct
-	// third ask question in terminal and get answers
-	// forth report quiz
+	fmt.Println(quiz)
 }
