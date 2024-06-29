68ae08cc1c9428d4e43a792910f3cd582ba70cb5
moecods <jakmesh96@gmail.com>
2024-06-29 17:06:15 +0330

    feat: add util func ResponseWithJSON

---
+++ utils/response.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func RespondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	}
+}
