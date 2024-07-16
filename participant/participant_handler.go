4220dbff43b47975e0540bcb0989de21d54ad794
moecods <jakmesh96@gmail.com>
2024-07-16 12:07:52 +0330

    docs: add api docs for save participants answers

--- participant/participant_handler.go
+++ participant/participant_handler.go
@@ -32,6 +32,17 @@ func NewParticipantHandler(repo ParticipantRepository) *ParticipantHandler {
 	return &ParticipantHandler{ParticipantRepo: repo}
 }
 
+// RegisterParticipantsHandler godoc
+//
+//	@Summary		Register participants
+//	@Description	Register participants
+//	@Tags			participants
+//	@Accept			json
+//	@Produce		json
+//	@Param			RegisterRequest	body		RegisterRequest	true	"Register Request object"
+//	@Failure		400				{string}	string			"Invalid request body"
+//	@Failure		500				{string}	string			"Failed to read request body"
+//	@Router			/participants/register [post]
 func (h *ParticipantHandler) RegisterParticipantsHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -43,7 +54,6 @@ func (h *ParticipantHandler) RegisterParticipantsHandler(w http.ResponseWriter, 
 	var RegisterRequest RegisterRequest
 
 	if err := json.Unmarshal(body, &RegisterRequest); err != nil {
-		log.Printf("Failed to unmarshal request body: %v", err)
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
@@ -60,12 +70,23 @@ func (h *ParticipantHandler) RegisterParticipantsHandler(w http.ResponseWriter, 
 	err = h.ParticipantRepo.AddManyParticipants(participants)
 
 	if err != nil {
-		log.Println(err)
 		http.Error(w, "Failed to store", http.StatusInternalServerError)
 	}
 	utils.RespondWithJSON(w, http.StatusOK, participants)
 }
 
+// RegisterParticipantsHandler godoc
+//
+//	@Summary		Save participants answers
+//	@Description	Save participants answers
+//	@Tags			participants
+//	@Accept			json
+//	@Produce		json
+//	@Param			SaveParticipantsAnswersRequest	body		SaveParticipantsAnswersRequest	true	"Save participants answers Request object"
+//	@Failure		404								{string}	string							"Participant Not Found"
+//	@Failure		400								{string}	string							"Invalid request body"
+//	@Failure		500								{string}	string							"Failed to read request body"
+//	@Router			/participants/answers [post]
 func (h *ParticipantHandler) SaveParticipantsAnswersHandler(w http.ResponseWriter, r *http.Request) {
 	var participantsAnswersRequest SaveParticipantsAnswersRequest
 
